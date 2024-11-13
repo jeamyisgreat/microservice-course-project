6ecf656356b3d9225433d0d0d6cb2b648c9af085
JN <nguyenjeamy@gmail.com>
2024-11-13 11:33:38 -0500

    created versions.tf

---
+++ versions.tf
@@ -0,0 +1,19 @@
+terraform {
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+      version = "5.75.1"
+    }
+  }
+}
+
+provider "aws" {
+  # Configuration options
+  region = "us-east-1"
+  default_tags {
+    tags = {
+        owner = "jnguyen"
+    }
+
+  }
+}
